b7d72b3e2b3c8c926e56f18dfe3f250a9c028d67
Ramzi Fartas <rfartas83@gmail.com>
2023-08-21 01:11:18 +0200

    modified:   next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  nextauth_url:'api/auth/callback/google',
+  env:{
+    nextauth_url:'api/auth/callback/google'
+  },
   images: {
     remotePatterns: [
       {
