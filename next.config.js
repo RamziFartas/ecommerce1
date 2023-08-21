67c9b41ae3c38c5869977f6193b06af59e20cfb1
Ramzi Fartas <rfartas83@gmail.com>
2023-08-21 17:23:51 +0200

    modified:   .env 	modified:   next.config.js 	modified:   pages/api/auth/[...nextauth].js

--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  env:{
-    nextauth_url:'api/auth/callback/google'
-  },
   images: {
     remotePatterns: [
       {
