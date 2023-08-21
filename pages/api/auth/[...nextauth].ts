67368df5d50c56bd2a4346f21338c253e2c2fa2e
Ramzi Fartas <rfartas83@gmail.com>
2023-08-21 20:53:47 +0200

    modified:   pages/api/auth/[...nextauth].ts

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,9 +1,15 @@
-import NextAuth, { getServerSession } from 'next-auth'
+import NextAuth, { Session, getServerSession } from 'next-auth'
 import GoogleProvider from 'next-auth/providers/google'
 import { MongoDBAdapter } from "@next-auth/mongodb-adapter"
 import clientPromise from "@/lib/mongodb"
 
 const adminEmails: string[] = ['rfartas83@gmail.com'];
+// Define the types for your user and session objects
+interface User {
+  email: string;
+  // Add other user properties here
+}
+
 
 const authOptions: any = {
   providers: [
@@ -29,7 +35,7 @@ export default NextAuth(authOptions);
 
 export async function isAdminRequest(req, res) {
   const session = await getServerSession(req, res, authOptions);
-  if (!adminEmails.includes(session?.user?.email)) {
+  if (!adminEmails.includes((session as any)?.user?.email)) {
     res.status(401);
     res.end();
     throw 'not an admin';
