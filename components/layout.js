c1f113a55b24135140184525f48c26138da33745
Ramzi Fartas <rfartas83@gmail.com>
2023-08-25 23:02:03 +0200

    modified:   components/layout.js 	renamed:    pages/api/auth/session/[...nextauth].js -> pages/api/auth/[...nextauth].js

--- components/layout.js
+++ components/layout.js
@@ -11,7 +11,7 @@ export default function Layout({children}) {
   <div className="text-center w-full text-gray-700">
   <button onClick={(e) => {
     e.preventDefault();
-    signIn('google')}} type="submit" variant="solid" className="p-2 px-4 bg-white rounded-lg">Login With Google</button>
+    signIn('google'),{ callbackUrl: REDIRECT_URL_AFTER_SIGN_IN }}} type="submit" variant="solid" className="p-2 px-4 bg-white rounded-lg">Login With Google</button>
   </div>
   </div>)
   }
